ce1ec6810e8f377c28d3cab6861dfe1e6e730312
zhidal <zhidal@qq.com>
2023-03-18 11:57:52 +0800

    docs: 📝 添加注释
    添加注释

---
+++ simple-rpc-javascript/lib/common/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Description  :
+ * @Author       : zhidal zhidal@qq.com
+ * @Date         : 2023-03-18 11:15:25
+ * @LastEditors  : zhidal zhidal@qq.com
+ * @LastEditTime : 2023-03-18 11:44:55
+ */
+
+export * from './protocol.js'
--- simple-rpc-javascript/lib/index.js
+++ simple-rpc-javascript/lib/index.js
@@ -1,2 +1,11 @@
-export * from './server/server.js';
-export * from './client/client.js';
+/*
+ * @Description  :
+ * @Author       : zhidal zhidal@qq.com
+ * @Date         : 2023-03-16 19:24:17
+ * @LastEditors  : zhidal zhidal@qq.com
+ * @LastEditTime : 2023-03-18 11:46:17
+ */
+
+export * from './server/index.js';
+export * from './client/index.js';
+export * from './common/index.js';
